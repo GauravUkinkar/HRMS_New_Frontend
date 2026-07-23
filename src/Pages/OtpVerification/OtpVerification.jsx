b44255e5478003c4006e5f5ef05b08447eb85e2a
VSD2001 <varad422001deshpande@gmail.com>
2026-07-23 12:03:27 +0530

    pp

--- src/Pages/OtpVerification/OtpVerification.jsx
+++ src/Pages/OtpVerification/OtpVerification.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import OtpVerification from "./OtpVerification.scss";
+import React from "react";
+import "./OtpVerification.scss";
+import Logo from "../../assets/PandozaLogo.png"
 
 const OtpVerification = () => {
   return (
     <>
-    <div className="verification-parent parent">
+      <div className="verification-parent parent">
         <div className="verification-cont cont">
-            
+          <div className="left">
+            <div className="logo">
+              <img src={Logo} alt="Logo" />
+            </div>
+            <h1>OTP VERIFICATION</h1>
+            <p>Please enter the 6-digit One Time <br/> Password sent to your email</p>
+          </div>
+          <div className="right"></div>
         </div>
-       
-    </div>
-      
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default OtpVerification
+export default OtpVerification;
