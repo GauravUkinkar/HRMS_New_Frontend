52d0226ff4697cf1b0dee6147692f505c3329703
aishwarya-0702 <aishwaryabhandare0702@gmail.com>
2026-07-24 11:47:40 +0530

    Refactor Login component layout and styles for improved structure and usability

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import "./login.scss";
 import Logo from "../../assets/logo.png";
 import login from "../../assets/login.webp";
@@ -18,33 +18,45 @@ const ChangePass = () => {
             <p>Have we met before?</p>
 
             <div className="inputs">
-            <div className="form-row">
-              <Input
-                text_color="white"
-                fc_color="white"
-                bd_color="white"
-                lb_color="white"
-                label="Email"
-              />
+              <div className="form-row">
+                <Input
+                  text_color="white"
+                  fc_color="white"
+                  bd_color="white"
+                  lb_color="white"
+                  label="Email"
+                />
+              </div>
+
+              <div className="form-row">
+                <Input
+                  text_color="white"
+                  type="password"
+                  fc_color="white"
+                  bd_color="white"
+                  lb_color="white"
+                  label="Password"
+                />
+              </div>
             </div>
+            <div className="password-footer">
+              <div className="rem">
+                 <input type="checkbox" className="checkbox"  />
+              <label className="remember">
+               
+                
+                Remember me
+              </label>
+              </div>
 
-
-            
-            <div className="form-row">
-              <Input
-                text_color="white"
-                type="password"
-                fc_color="white"
-                bd_color="white"
-                lb_color="white"
-                label="Password"
-              />
+              <a href="/" className="forgot-password">
+                Forgot my password
+              </a>
             </div>
+            <Link className="btn" to="#">
+              Log in
+            </Link>
           </div>
-            <Link className="btn" to="#" >Log in</Link>
-          </div>
-
-          
 
           <div className="right">
             <img src={login} alt="loginimage" />
