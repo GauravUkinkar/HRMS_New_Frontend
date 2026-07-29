8b1b113e8a702fbdda867685107fdac94e4bb3fd
Ketan Dudka <ketandudka566123@gmail.com>
2026-07-29 10:33:09 +0530

    Implement Context API for user authentication and create AuthRoute component for protected routes

---
+++ Context.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+
+export const UserContext = createContext();
+
+const ContextProvider = ({ children }) => {
+  return <UserContext.Provider>{children}</UserContext.Provider>;
+};
+
+export default ContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
+import { createRoot } from "react-dom/client";
 
-import { createRoot } from 'react-dom/client'
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ContextProvider from "../Context.js";
 
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-     
+createRoot(document.getElementById("root")).render(
+  <ContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  
-
-)
+  </ContextProvider>,
+);
