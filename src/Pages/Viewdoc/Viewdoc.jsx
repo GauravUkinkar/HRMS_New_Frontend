01f7e99b8f6b083c9aa32ac7cb2c310c674d7c34
aishwarya-0702 <aishwaryabhandare0702@gmail.com>
2026-07-30 16:36:30 +0530

    Add preview button to document cards and adjust layout styles for improved usability

--- src/Pages/Viewdoc/Viewdoc.jsx
+++ src/Pages/Viewdoc/Viewdoc.jsx
@@ -3,6 +3,7 @@ import "./Viewdoc.scss";
 import { GrDocumentPdf } from "react-icons/gr";
 import { IoMdDownload } from "react-icons/io";
 import { IoCloudUploadOutline } from "react-icons/io5";
+import { MdOutlinePreview } from "react-icons/md";
 import MainPanel from "../../comp/MainPanel/MainPanel";
 const Viewdoc = () => {
   return (
@@ -15,44 +16,74 @@ const Viewdoc = () => {
           <div className="document-card">
             <GrDocumentPdf className="pdf-icon" />
             <p>Adhar Card</p>
+            <div className="btn-parent">
+                <button>
+            <MdOutlinePreview />Preview
+            </button>
+            
             <button>
               <IoMdDownload /> Download
             </button>
+            </div>
           </div>
           <div className="document-card">
             <GrDocumentPdf className="pdf-icon" />
             <p>Pan Card</p>
+             <div className="btn-parent">
+                <button>
+            <MdOutlinePreview />Preview
+            </button>
             <button>
               <IoMdDownload /> Download
             </button>
+             </div>
           </div>
           <div className="document-card">
             <GrDocumentPdf className="pdf-icon" />
             <p>Experience Letter</p>
+             <div className="btn-parent"><button>
+            <MdOutlinePreview />Preview
+            </button>
             <button>
               <IoMdDownload /> Download
-            </button>
+            </button></div>
           </div>
           <div className="document-card">
             <GrDocumentPdf className="pdf-icon" />
             <p>Salary Slip 1</p>
+             <div className="btn-parent">
+                <button>
+            <MdOutlinePreview />Preview
+            </button>
             <button>
               <IoMdDownload /> Download
             </button>
+             </div>
           </div>
           <div className="document-card">
             <GrDocumentPdf className="pdf-icon" />
             <p>Salary slip 2</p>
+            <div className="btn-parent">
+                <button>
+            <MdOutlinePreview />Preview
+            </button>
+            
             <button>
               <IoMdDownload /> Download
             </button>
+            </div>
           </div>
           <div className="document-card">
             <GrDocumentPdf className="pdf-icon" />
             <p>Salary Slip 3</p>
+            <div className="btn-parent">
+                <button>
+            <MdOutlinePreview />Preview
+            </button>
             <button>
               <IoMdDownload /> Download
             </button>
+            </div>
           </div>
           
         </div>
