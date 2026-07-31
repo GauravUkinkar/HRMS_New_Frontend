55e18caf528d06a15737fce224a1e97ac70d3a6a
VSD2001 <varad422001deshpande@gmail.com>
2026-07-31 17:47:35 +0530

    Add birthday notifications and improve layout in AdminDash; enhance styling for better readability

--- src/Pages/AdminDash/AdminDash.jsx
+++ src/Pages/AdminDash/AdminDash.jsx
@@ -4,6 +4,7 @@ import "./AdminDash.scss";
 import { IoDocumentTextSharp } from "react-icons/io5";
 import { CiMenuKebab } from "react-icons/ci";
 import Calender from "../../comp/Calender/Calender";
+import { FaBirthdayCake } from "react-icons/fa";
 
 const AdminDash = () => {
   return (
@@ -34,56 +35,77 @@ const AdminDash = () => {
               <button className="btn">View Documents</button>
             </div>
             <div className="left2">
-                <div className="heading">
-                    <p>Total Hours Log-In</p>
-                    <p>45h 00m </p>
-                </div>
+              <div className="heading">
+                <p>Total Hours Log-In</p>
+                <p>45h 00m </p>
+              </div>
             </div>
             <div className="left3">
-                <div className="top">
-                    <h3>Notification</h3>
-                    <CiMenuKebab />
-                </div>
-                <div className="card">
-                    <div className="heading">Pramotion Review</div>
-                    <p>11 June 2026Discussed potential promotion in Q1based on consistent performance andleadership in the recentproject.</p>
-                </div>
-                <div className="card">
-                      <div className="heading">Employee Appreciation</div>
-                    <p>7 May 2026Recognized by the team and CEO foroutstanding contribution in the clientworkshop and delivery timeline.</p>
-                </div>
+              <div className="top">
+                <h3>Notification</h3>
+                <CiMenuKebab />
+              </div>
+              <div className="card">
+                <div className="heading">Pramotion Review</div>
+                <p>
+                  11 June 2026Discussed potential promotion in Q1based on
+                  consistent performance andleadership in the recentproject.
+                </p>
+              </div>
+              <div className="card">
+                <div className="heading">Employee Appreciation</div>
+                <p>
+                  7 May 2026Recognized by the team and CEO foroutstanding
+                  contribution in the clientworkshop and delivery timeline.
+                </p>
+              </div>
             </div>
           </div>
           <div className="middle">
             <div className="middle1"></div>
             <div className="middle2"></div>
-            <div className="middle3"></div>
+            <div className="middle3">
+              <h3>Team Status Overview</h3>
+
+            </div>
           </div>
           <div className="right">
             <div className="right1">
-               <h1 className="heading">
-                Attendence
-               </h1>
-                <Calender/>
-                <div className="bottom">
-                    <div className="absent">
-                      <div className="red"></div>
-                      <p>Absent</p>
-                    </div>
-                     <div className="today">
-                      <div className="blue"></div>
-                      <p>Today</p>
-                    </div>
-                     <div className="halfday">
-                      <div className="grey"></div>
-                      <p>Half Day</p>
-                    </div>
+              <h1 className="heading">Attendence</h1>
+              <Calender />
+              <div className="bottom">
+                <div className="absent">
+                  <div className="red"></div>
+                  <p>Absent</p>
                 </div>
+                <div className="today">
+                  <div className="blue"></div>
+                  <p>Today</p>
+                </div>
+                <div className="halfday">
+                  <div className="grey"></div>
+                  <p>Half Day</p>
+                </div>
+              </div>
             </div>
             <div className="right2">
-                <div className="top">
+              <div className="top">
+                <h3>Birthdays This Month</h3>
+                <FaBirthdayCake />
+              </div>
+              <div className="card">
+                <div className="name">
+                  <div className="user-avatar">SS</div>
 
+                  <div className="user-info">
+                    <p>Davis Lewis</p>
+                    <span>Accountant</span>
+                  </div>
                 </div>
+                <div className="date">
+                  <p>01-08-2026</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
