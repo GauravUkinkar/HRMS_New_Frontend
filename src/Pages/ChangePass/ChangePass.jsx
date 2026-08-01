2c571dd0578d761b895441992e48f6c5008c0f0b
Ketan Dudka <ketandudka566123@gmail.com>
2026-08-01 14:03:17 +0530

    Enhance ChangePass component; implement password update logic, improve error handling, and update UI elements

--- src/Pages/ChangePass/ChangePass.jsx
+++ src/Pages/ChangePass/ChangePass.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ChangePass.scss";
 import Logo from "../../assets/logo.png";
 import otpimage from "../../assets/rightimg.webp";
 import Input from "../../comp/input/Input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import UseForm from "../../UseForm";
 import { validatePassword } from "../../validators/ValidatePassword";
 import axios from "axios";
+import { toast } from "react-toastify";
+import { UserContext } from "../../../Context";
 
 const ChangePass = () => {
   const BASE_URL = import.meta.env.VITE_USER_BACKEND_URL;
@@ -16,10 +18,39 @@ const ChangePass = () => {
     confirmPassword: "",
   };
 
+  const {setLoader} = useContext(UserContext)
+
+  const [searchParams] =useSearchParams();
+  const navigate = useNavigate()
+
+  const email = searchParams.get("email")
+
   const updatePassword = async () => {
     try {
-      const response = await axios.put(`${BASE_URL}AuthController/updatePassword`,values)
-    } catch (error) {}
+      setLoader(true)
+      const response = await axios.put(`${BASE_URL}AuthController/updatePassword`,{
+        email:email,
+        newPassword:values.newPassword,
+        confirmPassword:values.confirmPassword
+      });
+
+      console.log(response);
+      if(response?.status === 200){
+        toast.success("Password Changed Successfully");
+        setValues({
+           newPassword: "",
+    confirmPassword: "",
+        });
+        navigate("/login")
+      }
+    } catch (error) {
+      console.log(error.response);
+      if(error?.response?.data?.responseMessage){
+        toast.error(error?.response?.data?.responseMessage)
+      }
+    }finally{
+      setLoader(false)
+    }
   };
 
   const {
@@ -32,6 +63,8 @@ const ChangePass = () => {
     setError,
     isSubmitting,
   } = UseForm(formObj, validatePassword, updatePassword);
+
+  
 
   return (
     <>
@@ -46,7 +79,7 @@ const ChangePass = () => {
               <p>
                 Please enter the 6-digit One Time <br /> Password sent to your
                 email <br />
-                <p className="mail">admin@gmail.com</p>
+                <p className="mail">{email}</p>
               </p>
             </div>
 
@@ -85,7 +118,7 @@ const ChangePass = () => {
               </div>
             </div>
             <button className="btn" type="submit">
-              Verify OTP
+              Submit
             </button>
             <div className="links">
               <Link to="/otpverification">Back To Previous Screen</Link>
