3a29124bf839bf926978190199f1a7abb28d213e
VSD2001 <varad422001deshpande@gmail.com>
2026-08-01 15:45:49 +0530

    Implement OTP verification logic; integrate axios for API call and enhance user feedback with toast notifications

--- src/Pages/OtpVerification/OtpVerification.jsx
+++ src/Pages/OtpVerification/OtpVerification.jsx
@@ -4,8 +4,16 @@ import Logo from "../../assets/PandozaLogo.png";
 import otpimage from "../../assets/rightimg.webp";
 import OTPInput from "react-otp-input";
 import { Link, useSearchParams } from "react-router-dom";
+import axios from "axios";
+import UseForm from "../../UseForm";
+import { useEffect,useContext } from "react";
+import { UserContext } from "../../../Context";
+import { toast } from "react-toastify";
+import {  useNavigate } from "react-router-dom";
+
 
 const OtpVerification = () => {
+
 
 const [otp,setOtp] =useState("")
 
@@ -13,14 +21,41 @@ const [otp,setOtp] =useState("")
   const email = searchParams.get("email");
   console.log(email)
 
+  const {setLoader} = useContext(UserContext);
+  const navigate = useNavigate()
+  const BASE_URL = import.meta.env.VITE_USER_BACKEND_URL;
+
+  const verifyOtp = async (e) =>{
+     e.preventDefault();
+    try{
+      console.log("otp")
+      
+      setLoader(true)
+      const response = await axios.post(
+         `${BASE_URL}AuthController/VerifyOtp?Email=${email}&Otp=${otp}`,
+         {},
+      );
+      if(response?.status === 200){
+        toast.success("Otp Verified Successfully");
+        navigate(`/changepass?email=${email}`)
+      }
+      console.log(response)
+    } catch(error) {
+      const message = error.response.data
+      if(message?.responseMessage === "Invalid OTP"){
+        toast.error(message?.responseMessage)
+      }
+    }finally{
+      setLoader(false)
+    }
+  };
 
 
-console.log(otp)
   return (
     <>
       <div className="verification-parent parent">
         <div className="verification-cont cont">
-          <div className="left">
+          <form className="left" onSubmit={verifyOtp}>
             <div className="logo">
               <img src={Logo} alt="Logo" />
             </div>
@@ -43,14 +78,14 @@ console.log(otp)
                 )}
               />
             </div>
-            <Link className="btn" to="/changepass">
+            <button className="btn" type="submit">
               Verify OTP
-            </Link>
+            </button>
             <div className="links">
               <Link>Resend OTP</Link>
-              <Link>Back to Login</Link>
+              <Link to="/login">Back to Login</Link>
             </div>
-          </div>
+          </form>
           <div className="right">
             <img src={otpimage} alt="otpimage" />
           </div>
